2337fad2d3d5b7b0526f6521e18268d63202d0b9
JP Tucker <jpt4373@utulsa.edu>
2022-01-24 15:09:50 -0600

    Cube setup
    Created cube prefab
    -created start of RandomCubes class

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/***
+ * Created by: JP Tucker
+ * Date: Jan 24, 2022
+ * Last Edit By: NA
+ * Last edit Day: Jan 24, 2022
+ * 
+ * Description: S[awn multiple cube prefabs into the scene.
+ ***/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
